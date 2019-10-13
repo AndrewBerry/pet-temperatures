11013010a00b52009e56cb1ec4100bb07f35e0c1
Andrew Berry <andrew.berry@outlook.com>
2019-10-13 15:07:51 -0700

    data point submission via firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,60 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.submitDataPoint = functions.https.onRequest(async (req, res) => {
+  const { high, low, humidity, userId, petId } = req.body;
+
+  if (
+    typeof high === "undefined" ||
+    typeof low === "undefined" ||
+    typeof humidity === "undefined" ||
+    typeof userId === "undefined" ||
+    typeof petId === "undefined"
+  ) {
+    res
+      .status(401)
+      .send({
+        success: false,
+        message:
+          "Missing data points. Required: high, low, humidity, userId and petId"
+      })
+      .end();
+
+    return;
+  }
+
+  const petDoc = admin.firestore().doc(`users/${userId}/pets/${petId}`);
+  const petSnapshot = await petDoc.get();
+  const petData = petSnapshot.data();
+
+  if (typeof petData === "undefined") {
+    res
+      .status(404)
+      .send({
+        success: false,
+        message: "Could not locate userId/petId combination."
+      })
+      .end();
+
+    return;
+  }
+
+  const temps = petData.temps || [];
+  const recentTemps = temps.filter(({ w }) => w > Date.now() - 86400000);
+
+  await petDoc.update({
+    temps: [
+      ...recentTemps,
+      {
+        w: Date.now(),
+        h: high,
+        l: low,
+        hu: humidity
+      }
+    ]
+  });
+
+  res.send({ success: true }).end();
+});
