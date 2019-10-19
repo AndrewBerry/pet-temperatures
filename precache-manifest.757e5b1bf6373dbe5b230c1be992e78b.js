0f2296159d191935f0740b7192454397b1cde4ac
Andrew Berry <andrew.berry@outlook.com>
2019-10-19 14:59:34 +1100

    Updates

---
+++ precache-manifest.757e5b1bf6373dbe5b230c1be992e78b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07de11e742f5ad23c733aed738339f9",
+    "url": "/pet-temperatures/index.html"
+  },
+  {
+    "revision": "eb5b1d2f6373b4f19dab",
+    "url": "/pet-temperatures/static/css/main.baadf225.chunk.css"
+  },
+  {
+    "revision": "482caa671aaf4c49885c",
+    "url": "/pet-temperatures/static/js/2.13fb5159.chunk.js"
+  },
+  {
+    "revision": "eb5b1d2f6373b4f19dab",
+    "url": "/pet-temperatures/static/js/main.43bdcfa6.chunk.js"
+  },
+  {
+    "revision": "826e22dde95aec4d4420",
+    "url": "/pet-temperatures/static/js/runtime-main.0799ead0.js"
+  }
+]);
